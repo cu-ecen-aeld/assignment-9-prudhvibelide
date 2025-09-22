ab8dc31ea75c14a91b8af6e38450ee4300fcc9c9
prudhvi-B007 <prudhvibelide9494@gmail.com>
2025-09-22 01:16:54 -0600

    Final Commit for Assignment-4 Part2

--- base_external/base_external/external.mk
+++
@@ -1,2 +0,0 @@
-# Include all package .mk files in this external tree
-include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/*/*.mk))
--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,1 @@
-# Include all package .mk files in this external tree
-include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))

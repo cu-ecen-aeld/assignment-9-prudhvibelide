e609c9a4e8a2c3533a81032a06141f7b0977cb73
prudhvi-B007 <prudhvibelide9494@gmail.com>
2025-10-11 21:56:11 -0600

    Assignment-7-Part2 Final Commit

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# ldd
+#
+################################################################################
+
+LDD_VERSION = 1.0
+LDD_SITE = /home/prbe/Documents/AESD/Assignment7/assignment-7-prudhvibelide
+LDD_SITE_METHOD = local
+LDD_DEPENDENCIES = linux
+
+define LDD_BUILD_CMDS
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(LDD_SITE)/scull" modules
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(LDD_SITE)/misc-modules" modules
+endef
+
+define LDD_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) \
+		M="$(LDD_SITE)/scull" \
+		INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) \
+		M="$(LDD_SITE)/misc-modules" \
+		INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
+endef
+
+
+
+$(eval $(generic-package))

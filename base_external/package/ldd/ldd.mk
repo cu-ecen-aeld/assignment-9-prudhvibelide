537fa88fe75f9056abe716dfb5ef08e6e4e36995
prudhvi-B007 <prudhvibelide9494@gmail.com>
2025-10-12 10:34:49 -0600

    Fix include path for ldd headers

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,15 +4,14 @@
 #
 ################################################################################
 
-
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-prudhvibelide.git
-LDD_SITE_METHOD = git
 LDD_VERSION = 8448dc2a33951689d208a220d5fe2be1d804c438
+LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-prudhvibelide.git
+LDD_SITE_METHOD = git
 LDD_DEPENDENCIES = linux
 
 define LDD_BUILD_CMDS
-	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)/scull" modules
-	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)/misc-modules" modules
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)/scull" EXTRA_CFLAGS="-I$(@D)/include" modules
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)/misc-modules" EXTRA_CFLAGS="-I$(@D)/include" modules
 endef
 
 define LDD_INSTALL_TARGET_CMDS
@@ -25,3 +24,4 @@ define LDD_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
+

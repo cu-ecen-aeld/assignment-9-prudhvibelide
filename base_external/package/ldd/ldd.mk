fed396c0fab924365502ef5cde2d7bf6258fe932
prudhvi-B007 <prudhvibelide9494@gmail.com>
2025-10-12 10:30:09 -0600

    Changed ldd.mk file format

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,25 +4,24 @@
 #
 ################################################################################
 
+
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-prudhvibelide.git
+LDD_SITE_METHOD = git
 LDD_VERSION = 8448dc2a33951689d208a220d5fe2be1d804c438
-LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-prudhvibelide.git
-LDD_SITE_METHOD = local
 LDD_DEPENDENCIES = linux
 
 define LDD_BUILD_CMDS
-	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(LDD_SITE)/scull" modules
-	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(LDD_SITE)/misc-modules" modules
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)/scull" modules
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)/misc-modules" modules
 endef
 
 define LDD_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) \
-		M="$(LDD_SITE)/scull" \
+		M="$(@D)/scull" \
 		INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
 	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) \
-		M="$(LDD_SITE)/misc-modules" \
+		M="$(@D)/misc-modules" \
 		INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
 endef
-
-
 
 $(eval $(generic-package))
